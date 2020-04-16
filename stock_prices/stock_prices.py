7a3b2471f5a8f325628a0ad5628eddcf27ab27e0
sumeyaali <thesumeyaali@gmail.com>
2020-04-16 12:30:39 -0700

    done with recipe

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,37 @@
 
 import argparse
 
+# PLAN
+# Store 3 values
+# FIRST:
+# The price to hold (starting at index 0)
+# SECOND:
+# The 2nd price that will be compared to price 1 to see if it's going to yield a profit
+# Third:
+# The profit, initialized to 0 
+
+# loop through array first time looking at initial price (i)
+# loop through array again looking at subsequent prices to see the maximum profit (j)
+# Once maximum profit is found, set that number to profit
+# MUST BUY BEFORE YOU CAN SELL
+
+
 def find_max_profit(prices):
-  pass
+  profit = 0
+  for i in range(len(prices) -1 ):  # giving us indices in prices array 
+    print(f'THIS IS I',i)
+     # j is iterating through the array to 
+    for j in range(i + 1, len(prices)):
+      if prices[j] >= prices[i]:
+        profit = prices[j] - prices[i]
+        # set a max profit variable and compare it to profit if max profit is < then set profit to max profit
+        print(f'Profit: ',profit)
+
+    return profit
+
+prices = [1050, 270, 1540, 3800, 2, 5000]
+print(F"Max profit: ",find_max_profit(prices))
+
 
 
 if __name__ == '__main__':
